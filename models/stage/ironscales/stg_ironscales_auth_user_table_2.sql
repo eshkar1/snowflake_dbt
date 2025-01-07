177e06c615bf99a3ea19732c5b283afb56176b7e
Daniel Eshkar <deshkar@ironscales.com>
2025-01-07 09:52:21 +0000

    added incremental to auth user table

---
+++ models/stage/ironscales/stg_ironscales_auth_user_table_2.sql
@@ -0,0 +1,23 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
+with source as (
+    select * from {{ source('ironscales_us','auth_user_table') }}
+),
+
+renamed as (
+    select
+    *
+    from source
+
+{% if is_incremental() %}
+
+where _rivery_last_update > (select max(_rivery_last_update) from {{this}})
+
+{% endif %}
+)
+
+select *  from renamed
