0342bef5832c71da71ea11c87d2be160db4c934f
Daniel Eshkar <deshkar@ironscales.com>
2025-01-12 10:14:46 +0000

    delete py

--- models/test_python/python.py
+++
@@ -1,8 +0,0 @@
-import snowflake.snowpark.function as f
-
-def model(dbt,session):
-    df = session.sql("select * from FINANCE_DB.BILLING_SCH.GLOBAL_TENANT_HISTORY_4BILLING_TBL")
-
-    df_filter = df.filter(f.col("ROOT") == 'US-733')
-    
-return df_filter
