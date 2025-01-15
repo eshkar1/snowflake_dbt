64f6cb091c990f665e52e65337a4ea4c70a36410
Daniel Eshkar <deshkar@ironscales.com>
2025-01-15 07:52:10 +0000

    a

--- models/conform/ironscales/current_month_ltp_roundup_tbl.sql
+++ models/conform/ironscales/current_month_ltp_roundup_tbl.sql
@@ -6,10 +6,7 @@ with global_tenant_history as (
 ltp_pricing_list as (
     select * from {{ ref('ltp_pricing_tbl')}}
 )
--- ,
 
-
---  current_month_ltp_roundup_tbl AS (
     SELECT
         max_by(tenant_global_id, active_profiles) AS tenant_global_id,
         max_by(record_date, active_profiles) AS record_date
@@ -20,13 +17,13 @@ ltp_pricing_list as (
         AND approved = true
         AND billing_status = 'Active'
         AND root IN (
-            SELECT
-                tenant_global_id
-            FROM
-                ltp_pricing_list
-            WHERE
-                is_highwatermark = true
-        )
+                    SELECT
+                        tenant_global_id
+                    FROM
+                        ltp_pricing_list
+                    WHERE
+                        is_highwatermark = true
+                    )
     GROUP BY
         tenant_global_id
     
@@ -42,12 +39,12 @@ ltp_pricing_list as (
         AND approved = true
         AND billing_status = 'Active'
         AND root IN (
-            SELECT
-                tenant_global_id
-            FROM
-                ltp_pricing_list
-            WHERE
-                is_highwatermark = false
-        )
+                    SELECT
+                        tenant_global_id
+                    FROM
+                        ltp_pricing_list
+                    WHERE
+                        is_highwatermark = false
+                      )
     GROUP BY
         tenant_global_id
