624ecb2bd889ea711b548ff5904a9da7df2bfbe2
Daniel Eshkar <deshkar@ironscales.com>
2025-05-11 08:15:04 +0000

    s

--- models/prod/ironscales/daily_billable_mailboxes_tbl_US.sql
+++ models/prod/ironscales/daily_billable_mailboxes_tbl_US.sql
@@ -14,11 +14,11 @@ ltp_pricing_list as (
     select * from {{ ref('ltp_pricing_tbl')}}
 )
 
-select
-*
-from PROD_CONFORM.DBT_PROD_DB.DAILY_BILLABLE_MAILBOXES_TBL_US
+-- select
+-- *
+-- from PROD_CONFORM.DBT_PROD_DB.DAILY_BILLABLE_MAILBOXES_TBL_US
 
-union 
+-- union 
 
 select
 current_date as record_date,
