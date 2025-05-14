f30b24809eead41789acf5b70cf0aa453d7753a8
Daniel Eshkar <deshkar@ironscales.com>
2025-05-14 08:54:01 +0000

    d

--- models/prod/ironscales/global_tenant_history_daily_agg_billing_tbl.sql
+++ models/prod/ironscales/global_tenant_history_daily_agg_billing_tbl.sql
@@ -2,11 +2,11 @@ with global_tenant_history_daily_billing_tbl as (
     select * from {{ ref('global_tenant_history_daily_billing_tbl')}}
 )
 
-select
-*
-from PROD_CONFORM.DBT_PROD_DB.global_tenant_history_daily_agg_billing_tbl
+-- select
+-- *
+-- from PROD_CONFORM.DBT_PROD_DB.global_tenant_history_daily_agg_billing_tbl
 
-union 
+-- union 
 
 select
 *
