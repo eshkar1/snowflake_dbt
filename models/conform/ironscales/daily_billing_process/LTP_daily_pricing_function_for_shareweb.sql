a44cc33664449bad620ede9b865f815f0483afde
Daniel Eshkar <deshkar@ironscales.com>
2025-06-04 09:06:27 +0000

    s

--- models/conform/ironscales/daily_billing_process/LTP_daily_pricing_function_for_shareweb.sql
+++ models/conform/ironscales/daily_billing_process/LTP_daily_pricing_function_for_shareweb.sql
@@ -8,7 +8,8 @@ ltp_pricing_list as (
     select * from 
     -- prod_mart.upload_tables.ltp_pricing_list
     {{ ref('ltp_pricing_tbl')}}
-    and IS_TRACKED = true
+    where
+    IS_TRACKED = true
 )
 
 
