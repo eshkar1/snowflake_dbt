95d5b5314d87fcfd7a1116a5cd5b73efe119ac2c
Daniel Eshkar <deshkar@ironscales.com>
2025-10-29 14:47:11 +0000

    adding gmails and irontrapsetting tables

---
+++ models/stage/ironscales/stg_ironscales_gmailwebhookintegration_table.sql
@@ -0,0 +1,18 @@
+with source as (
+    select * from {{ source('ironscales_us','ironapi_gmailwebhookintegration_table') }}
+),
+
+renamed as (
+            select
+            *
+            from source
+
+            {% if is_incremental() %}
+
+            where _rivery_last_update > (select max(_rivery_last_update) from {{this}})
+
+            {% endif %}
+
+            )
+
+select *  from renamed
